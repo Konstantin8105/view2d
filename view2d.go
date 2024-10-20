af7a498a6b319a1ce524240975042220b7cc239a
konstantin8105 <konstantin8105@gmail.com>
2024-10-20 18:59:25 +0300

    TODO

--- view2d.go
+++ view2d.go
@@ -6,10 +6,23 @@ import (
 	"math/rand"
 
 	"github.com/Konstantin8105/gog"
+	"github.com/Konstantin8105/pow"
 )
 
 type Vector struct {
 	start, finish gog.Point
+}
+
+func (v *Vector) Scale(scale float64) {
+	v.finish.X = v.start.X + (v.finish.X-v.start.X)*scale
+	v.finish.Y = v.start.Y + (v.finish.Y-v.start.Y)*scale
+}
+
+func (v *Vector) Rotate(rand float64) {
+	if rand < 0 || 1 < rand {
+		panic(fmt.Errorf("not valid random value: %.5f", rand))
+	}
+	// TODO rotate at random angle from -90 ... +90 degree
 }
 
 type Curve interface {
@@ -27,17 +40,18 @@ func (l Line) GetVector(rand float64) (v Vector) {
 	if rand < 0 || 1 < rand {
 		panic(fmt.Errorf("not valid random value: %.5f", rand))
 	}
-	v.start.X = p1.X + (p1.X-p2.X)*rand
-	v.start.Y = p1.Y + (p1.Y-p2.Y)*rand
+	v.start.X = l.p1.X + (l.p2.X-l.p1.X)*rand
+	v.start.Y = l.p1.Y + (l.p2.Y-l.p1.Y)*rand
 
+	// TODO v.finish perpendicular vector 1 size
 	return
 }
 
 func (l Line) Box() (begin, finish gog.Point) {
-	begin.X = min(p1.X, p2.X)
-	begin.Y = min(p1.Y, p2.Y)
-	finish.X = max(p1.X, p2.X)
-	finish.Y = max(p1.Y, p2.Y)
+	begin.X = min(l.p1.X, l.p2.X)
+	begin.Y = min(l.p1.Y, l.p2.Y)
+	finish.X = max(l.p1.X, l.p2.X)
+	finish.Y = max(l.p1.Y, l.p2.Y)
 	return
 }
 
@@ -48,17 +62,16 @@ func Calc(curves []Curve) {
 	return
 }
 
-var Amount = 1000
+var Amount int64 = 100
 
-func OneCurve(index int, curves []Curve) (viewFactor []float64) {
-	vf := make([]curves, len(curves))
-	present := curves[index]
+func OneCurve(present Curve, curves []Curve) (viewFactor []float64) {
+	vf := make([]int64, len(curves))
 	// calculate scale of geometry
 	scale := 0.0
 	{
 		var begin, finish gog.Point
 		for i := range curves {
-			b, f := curves.Box()
+			b, f := curves[i].Box()
 			begin.X = min(begin.X, b.X, f.X)
 			begin.Y = min(begin.Y, b.Y, f.Y)
 			finish.X = max(finish.X, b.X, f.X)
@@ -66,15 +79,17 @@ func OneCurve(index int, curves []Curve) (viewFactor []float64) {
 		}
 		scale = math.Sqrt(pow.E2(begin.X-finish.X) + pow.E2(begin.Y-finish.Y))
 	}
-	for iter := 0; iter <= Amount; iter++ {
+	for iter := int64(0); iter <= Amount; iter++ {
 		v := present.GetVector(rand.Float64())
 		// scale vector
 		v.Scale(scale)
-		// find intersection between vector and curves
+		// rotate vector
+		v.Rotate(rand.Float64())
+		// TODO find intersection between vector and curves
 
-		// get first intersection
+		// TODO get first intersection
 
-		// add to intersection view factor
+		// TODO add to intersection view factor
 	}
 	for i := range viewFactor {
 		viewFactor[i] = float64(vf[i]) / float64(Amount)
--- view2d_test.go
+++ view2d_test.go
@@ -8,16 +8,16 @@ import (
 )
 
 func Test(t *testing.T) {
-	vf := OneCurve(0, []Curve{
-		Line{
-			p1: gog.Point{X: 0, Y: 0},
-			p2: gog.Point{X: 1, Y: 0},
-		},
-		Line{
-			p1: gog.Point{X: 0, Y: 1},
-			p2: gog.Point{X: 0, Y: 0},
-		},
-	})
+	l1 := Line{
+		p1: gog.Point{X: 0, Y: 0},
+		p2: gog.Point{X: 1, Y: 0},
+	}
+	l2 := Line{
+		p1: gog.Point{X: 0, Y: 1},
+		p2: gog.Point{X: 0, Y: 0},
+	}
+	vf := OneCurve(l1, []Curve{l1, l2})
 	fmt.Println("expect") // TODO
 	fmt.Println(vf)
+	// TODO verification tests
 }
